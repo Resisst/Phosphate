26e0e04777b4ecb3b0f1871ad6974a8457de550a
Resist <93611543+Resisst@users.noreply.github.com>
2022-03-27 22:08:37 +0100

    Updated obfuscation

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import phosphate
 from colorama import Fore
+from codecs import encode as c_encode
 
+reverse_shuffle = """
+def f(s:str):
+    if len(s) % 4 == 0:
+        l = int(len(s) / 2)
+        s = s[l:] + s[:l]
+        l = int(l / 2)
+        s = s[l:] + s[:l]
+        l = l * 2
+        s = s[l:] + s[:l]
+        return s
+    else:
+        return False"""
 deobfuscate_function = """
 from base64 import b64decode as t
 from string import digits as p
@@ -45,6 +58,15 @@ def r(s:str):
             n += i
     return t(n.encode("utf8")).decode()
 """
+def shuffle(s:str):
+    if len(s) % 4 == 0:
+        l = int(len(s) / 4)
+        s = s[l:] + s[:l]
+        l = l * 2
+        s = s[l:] + s[:l]
+        return s
+    else:
+        return False
 
 def obfuscate():
     file_name = input(f"{Fore.GREEN}[{Fore.CYAN}>>>{Fore.GREEN}] {Fore.RESET}Enter the name of the file to obfuscate: ")
@@ -57,14 +79,23 @@ def obfuscate():
         for i in code_list:
             if i.startswith("import"):
                 imports.append(i)
+        imports = "".join(imports)
         code_list = [i for i in code_list if not i.startswith("import")]
         code = "".join(code_list)
         obfuscated_code = phosphate.encode(code)
+        layer1 = deobfuscate_function+'\n'+f"exec(r(\"{obfuscated_code}\"))"
+        layer2 = c_encode(layer1.encode("utf8"), "uu").decode()
+        while len(layer2) % 4 != 0:
+            layer2 += "~"
+        layer3_shuffled = shuffle(layer2).encode("utf8")
+        layer3_compression = c_encode(layer3_shuffled, "zip")
         with open(encrypted_file_name, "w") as f:
-            for i in imports:
-                f.write(i)
-            f.write(deobfuscate_function + '\n')
-            f.write(f"exec(r(\"{obfuscated_code}\"))")
+            if len(imports) > 1:
+                f.write(f"from codecs import decode as c_decode\n{imports}\n{reverse_shuffle}\nexec(c_decode(f(c_decode({layer3_compression}, 'zip').decode()).replace('~', '').encode('utf8'), 'uu').decode())")
+            else:
+                f.write(f"from codecs import decode as c_decode\n{reverse_shuffle}\nexec(c_decode(f(c_decode({layer3_compression}, 'zip').decode()).replace('~', '').encode('utf8'), 'uu').decode())")
+
+
         print(f"{Fore.GREEN}[{Fore.CYAN}>>>{Fore.GREEN}] {Fore.RESET}Obfuscated file saved as {encrypted_file_name}")
 
 obfuscate()
