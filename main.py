a347e85891ec7704b1a6ff8a7e6b467363d9b010
Resist <93611543+Resisst@users.noreply.github.com>
2022-03-27 22:59:07 +0100

    Fixes

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ def f(s:str):
     else:
         return False"""
 deobfuscate_function = """
-from base64 import b64decode as t
-from string import digits as p
-from string import ascii_letters as o
 
 h = p + o
 def b(s:str):
@@ -58,6 +55,9 @@ def r(s:str):
             n += i
     return t(n.encode("utf8")).decode()
 """
+deobfuscate_function_imports = """from base64 import b64decode as t
+from string import digits as p
+from string import ascii_letters as o"""
 def shuffle(s:str):
     if len(s) % 4 == 0:
         l = int(len(s) / 4)
@@ -80,6 +80,7 @@ def obfuscate():
             if i.startswith("import"):
                 imports.append(i)
         imports = "".join(imports)
+        imports += deobfuscate_function_imports
         code_list = [i for i in code_list if not i.startswith("import")]
         code = "".join(code_list)
         obfuscated_code = phosphate.encode(code)
@@ -90,10 +91,8 @@ def obfuscate():
         layer3_shuffled = shuffle(layer2).encode("utf8")
         layer3_compression = c_encode(layer3_shuffled, "zip")
         with open(encrypted_file_name, "w") as f:
-            if len(imports) > 1:
-                f.write(f"from codecs import decode as c_decode\n{imports}\n{reverse_shuffle}\nexec(c_decode(f(c_decode({layer3_compression}, 'zip').decode()).replace('~', '').encode('utf8'), 'uu').decode())")
-            else:
-                f.write(f"from codecs import decode as c_decode\n{reverse_shuffle}\nexec(c_decode(f(c_decode({layer3_compression}, 'zip').decode()).replace('~', '').encode('utf8'), 'uu').decode())")
+            f.write(f"from codecs import decode as c_decode\n{imports}\n{reverse_shuffle}\nexec(c_decode(f(c_decode({layer3_compression}, 'zip').decode()).replace('~', '').encode('utf8'), 'uu').decode())")
+            
 
 
         print(f"{Fore.GREEN}[{Fore.CYAN}>>>{Fore.GREEN}] {Fore.RESET}Obfuscated file saved as {encrypted_file_name}")
